2c413778e0984d7e3d937b9c9568fc8c474945b2
Andrés Trujillo Mateus <atrujillomateus@gmail.com>
2023-11-17 10:43:33 -0500

    This timeout should've been longer from the start. Srry.

--- _functions.js
+++ _functions.js
@@ -2,7 +2,7 @@
 
 function destroy_modal_front() {
     $("#modal_front").modal("hide");
-    setTimeout(() => $("#modal_front").remove(), 111);
+    setTimeout(() => $("#modal_front").remove(), 999);
 }
 
 function show_modal_front($state = "success", $title = "INFO.", $message = "Message.", $hideCancelBtn = false, $redirect = "javascript:destroy_modal_front();") {
