60aaa2be843b8cefeef1277f9d5170315315b518
Mateus <atrujillomateus@gmail.com>
2024-02-26 00:58:43 -0500

    Cleanup. Improve routes declaration through file.

--- home.php
+++ home.php
@@ -3,26 +3,7 @@
 $setLocalStorage = true;
 require_once "./_var.php";
 require_once $TO_HOME . "_functions.php";
-$routes = [
-    // "/uri" => ["URI" => "/file.php", "GET" => [..."key" => "value"...], "POST" => [..."key" => "value"...], "COMPONENT" => [..."#id" => "file.php"...]],
-    // "/" routes
-    "${ROOT_ROUTE}" => ["URI" => "/home.v2.php"],
-    "${ROOT_ROUTE}new" => ["URI" => "/home.v2.php"],
-    "${ROOT_ROUTE}v2" => ["URI" => "/home.v2.php"],
-    "${ROOT_ROUTE}es" => ["URI" => "/home.v2.php", "GET" => ["lang" => "es"]],
-    "${ROOT_ROUTE}en" => ["URI" => "/home.v2.php", "GET" => ["lang" => "en"]],
-    "${ROOT_ROUTE}dark" => ["URI" => "/home.v2.php", "GET" => ["theme" => "dark", "lang" => "en"]],
-    "${ROOT_ROUTE}light" => ["URI" => "/home.v2.php", "GET" => ["theme" => "light", "lang" => "en"]],
-    "${ROOT_ROUTE}oscuro" => ["URI" => "/home.v2.php", "GET" => ["theme" => "dark", "lang" => "es"]],
-    "${ROOT_ROUTE}claro" => ["URI" => "/home.v2.php", "GET" => ["theme" => "light", "lang" => "es"]],
-    "${ROOT_ROUTE}old" => ["URI" => "/home.v1.php"],
-    "${ROOT_ROUTE}v1" => ["URI" => "/home.v1.php"],
-    "${ROOT_ROUTE}v1.es" => ["URI" => "/home.v1.php", "GET" => ["lang" => "es"]],
-    "${ROOT_ROUTE}v1.en" => ["URI" => "/home.v1.php", "GET" => ["lang" => "en"]],
-    "${ROOT_ROUTE}resume" => ["FILE" => "resume.es.pdf"],
-    "${ROOT_ROUTE}resume.es" => ["FILE" => "resume.es.pdf"],
-    "${ROOT_ROUTE}resume.en" => ["FILE" => "resume.en.pdf"]
-];
+require_once $TO_HOME . "_routes.php";
 require_once $TO_HOME . "_router.php";
 require_once $TO_HOME . "common.php";
 ?>
