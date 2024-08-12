83ed515ee5327591976ffdf6913133014ba0c6a5
Mateus <atrujillomateus@gmail.com>
2024-08-11 20:55:42 -0500

    Create mail templating.

---
+++ mail/_test.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../../_var.php";
+require_once $TO_HOME . "_functions.php";
+//require_once $TO_HOME . "_plugin.php";
+//require_once $TO_HOME . "_config.php";
+//require_once $TO_HOME . "_routes.php";
+//require_once $TO_HOME . "_router.php";
+//require_once $TO_HOME . "_auth.php";
+//require_once $TO_HOME . "common.php";
+require_once "./mail.common.php";
+// --- PHP ---
+//die($mail_html);
