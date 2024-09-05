c770dfbdbd04a5935fd71b4cc0f05fabc882bf07
Mateus <atrujillomateus@gmail.com>
2024-09-05 01:27:31 -0500

    Firefox event.

--- js/v2.js
+++ js/v2.js
@@ -11,8 +11,9 @@ $(() => {
 	// Section scroll on non-scrollable body
 	$("section").each(function () {
 		$(this)
-			.off("mousewheel")
-			.on("mousewheel", function (e) {
+			.off("wheel")
+			.on("wheel", function (e) {
+				console.log("scroll");
 				const delta = e.originalEvent.deltaY;
 				const scrollSum = $(this).scrollTop() + $(this).innerHeight();
 				if (delta > 0) {
@@ -21,14 +22,14 @@ $(() => {
 					if (!$(nextSection).length) return;
 					$(`html, body, ${byCommon.APP_CONTAINER_SELECTOR ?? ""}`)
 						.stop()
-						.animate({ scrollTop: nextSection.offset().top - 1 }, 990, "easeInOutExpo");
+						.animate({ scrollTop: nextSection.offset().top - 1 }, 999, "easeInOutExpo");
 				} else if (delta < 0) {
 					if (scrollSum > $(this).innerHeight()) return;
 					const prevSection = $(this).prev("section");
 					if (!$(prevSection).length) return;
 					$(`html, body, ${byCommon.APP_CONTAINER_SELECTOR ?? ""}`)
 						.stop()
-						.animate({ scrollTop: prevSection.offset().top - 1 }, 990, "easeInOutExpo");
+						.animate({ scrollTop: prevSection.offset().top - 1 }, 999, "easeInOutExpo");
 				}
 			});
 	});
