6a5e4eaa1109d5cd43608b27c9048f9a4206f023
[Mateus] byUwUr <atrujillomateus@gmail.com>
2025-02-20 03:26:52 -0500

    Rename bywr-sidebar.

--- sidebar.old.php
+++ sidebar.old.php
@@ -9,58 +9,58 @@ require_once $TO_HOME . "_routes.php";
 //require_once $TO_HOME . "_auth.php";
 // --- PHP ---
 ?>
-<nav id="sidebar" class="sidebar accordion bywr-accordion bg-dark-transparent bg-blurred text-white">
+<nav id="bywr-sidebar" class="bywr-sidebar accordion bywr-accordion bg-dark-transparent bg-blurred text-white">
     <div class="overlay"></div>
-    <div class="sidebar-content accordion-item flex-grow-1">
-        <div class="sidebar-option p-2o5">
+    <div class="bywr-sidebar-content accordion-item flex-grow-1">
+        <div class="bywr-sidebar-option p-2o5">
             <div class="navbar-brand has-background-contain" style="height:48px;width:48px;background-image:url('<?= $HOME_PATH; ?>/img/logo.png');"></div>
             <span class="ms-2 me-5 pe-5">[Mateus] byUwUr v1</span>
         </div>
-        <a class="sidebar-option" href="#about">
+        <a class="bywr-sidebar-option" href="#about">
             <i class="fas fa-house"></i><span><?= $_helloworld; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
-        <a class="sidebar-option" href="#skills">
+        <a class="bywr-sidebar-option" href="#skills">
             <i class="fas fa-screwdriver-wrench"></i><span><?= $_skills; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
-        <a class="sidebar-option" href="#experience">
+        <a class="bywr-sidebar-option" href="#experience">
             <i class="fas fa-briefcase"></i><span><?= $_experience; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
-        <a class="sidebar-option" href="#education">
+        <a class="bywr-sidebar-option" href="#education">
             <i class="fas fa-graduation-cap"></i><span><?= $_education; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
-        <a class="sidebar-option" href="#awards">
+        <a class="bywr-sidebar-option" href="#awards">
             <i class="fas fa-folder-tree"></i><span><?= $_portfolio; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
-        <a class="sidebar-option" href="#contact">
+        <a class="bywr-sidebar-option" href="#contact">
             <i class="fas fa-envelope"></i><span><?= $_contact; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
     </div>
-    <div class="sidebar-content accordion-item flex-grow-0">
+    <div class="bywr-sidebar-content accordion-item flex-grow-0">
         <div class="d-flex flex-row">
-            <a class="sidebar-option" href="<?= $ROOT_ROUTE; ?>resume.es">
+            <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE; ?>resume.es">
                 <i class="fas fa-file-invoice"></i><img src="img/co.png" width="16px" height="12px" style="margin: 4px;" alt="" /><span>HDV</span><i class="fas fa-download ms-auto"></i>
             </a>
-            <a class="sidebar-option" href="<?= $ROOT_ROUTE; ?>resume.en">
+            <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE; ?>resume.en">
                 <i class="fas fa-file-invoice"></i><img src="img/uk.png" width="16px" height="12px" style="margin: 4px;" alt="" /><span>CV</span><i class="fas fa-download ms-auto"></i>
             </a>
         </div>
         <button class="accordion-header accordion-button p-2o5 collapsed" data-bs-toggle="collapse" data-bs-target="#lang-drop" aria-expanded="false" aria-controls="lang-drop">
             <i class="fas fa-earth-americas"></i><span><?= $_langs; ?></span>
         </button>
-        <div id="lang-drop" class="accordion-collapse collapse bg-dark-transparent" data-bs-parent="#sidebar">
+        <div id="lang-drop" class="accordion-collapse collapse bg-dark-transparent" data-bs-parent="#bywr-sidebar">
             <div class="d-flex flex-row">
-                <a class="sidebar-option" href="<?= $ROOT_ROUTE; ?>v1/es" title="Español"><img src="<?= $HOME_PATH; ?>/img/co.png" width="16px" height="12px" style="margin: 4px;" alt="" /> ESPAÑOL<i class="fas fa-angle-right ms-auto"></i></a>
-                <a class="sidebar-option" href="<?= $ROOT_ROUTE; ?>v1/en" title="English"><img src="<?= $HOME_PATH; ?>/img/uk.png" width="16px" height="12px" style="margin: 4px;" alt="" /> ENGLISH<i class="fas fa-angle-right ms-auto"></i></a>
+                <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE; ?>v1/es" title="Español"><img src="<?= $HOME_PATH; ?>/img/co.png" width="16px" height="12px" style="margin: 4px;" alt="" /> ESPAÑOL<i class="fas fa-angle-right ms-auto"></i></a>
+                <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE; ?>v1/en" title="English"><img src="<?= $HOME_PATH; ?>/img/uk.png" width="16px" height="12px" style="margin: 4px;" alt="" /> ENGLISH<i class="fas fa-angle-right ms-auto"></i></a>
             </div>
-            <!--a class="sidebar-option" href="javascript:;"><i class="fas fa-home"></i>Home<i class="fas fa-angle-right ms-auto"></i></a-->
+            <!--a class="bywr-sidebar-option" href="javascript:;"><i class="fas fa-home"></i>Home<i class="fas fa-angle-right ms-auto"></i></a-->
         </div>
-        <a class="sidebar-option" href="<?= $ROOT_ROUTE; ?>">
+        <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE; ?>">
             <i class="fas fa-wand-magic-sparkles"></i><span><?= $_new; ?> (v2)</span><i class="fas fa-angle-right ms-auto"></i>
         </a>
         <p class="m-0 p-2 border-top" style="font-size: 0.75rem;"><?= $_footer; ?></p>
     </div>
-    <a id="sidebar-toggle" class="sidebar-toggle" href="javascript:;" title="Alternar menu lateral"><i class="fas fa-bars"></i><span>menu</span></a>
-    <div id="sidebar-hidden" class="sidebar-hidden">
+    <a id="bywr-sidebar-toggle" class="bywr-sidebar-toggle" href="javascript:;" title="Alternar menu lateral"><i class="fas fa-bars"></i><span>menu</span></a>
+    <div id="bywr-sidebar-hidden" class="bywr-sidebar-hidden">
         <div class="navbar-brand has-background-contain mt-auto" style="height:48px;width:48px;background-image:url('<?= $HOME_PATH; ?>/img/logo.png');"></div>
     </div>
 </nav>
--- sidebar.php
+++ sidebar.php
@@ -9,46 +9,46 @@ require_once $TO_HOME . "_routes.php";
 //require_once $TO_HOME . "_auth.php";
 // --- PHP ---
 ?>
-<nav id="sidebar" class="sidebar accordion bywr-accordion bg-dark-transparent bg-blurred text-white">
+<nav id="bywr-sidebar" class="bywr-sidebar accordion bywr-accordion bg-dark-transparent bg-blurred text-white">
     <div class="overlay" style="opacity:0.2;"></div>
-    <div class="sidebar-content accordion-item flex-grow-1">
-        <div class="sidebar-option p-2o5">
+    <div class="bywr-sidebar-content accordion-item flex-grow-1">
+        <div class="bywr-sidebar-option p-2o5">
             <div class="navbar-brand has-background-contain" style="height:48px;width:48px;background-image:url('<?= $HOME_PATH; ?>/img/logo.png');"></div>
             <span class="ms-2 me-5 pe-5">[Mateus] byUwUr</span>
         </div>
-        <a class="sidebar-option" href="#about">
+        <a class="bywr-sidebar-option" href="#about">
             <i class="fas fa-house"></i><span><?= $_helloworld; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
-        <a class="sidebar-option" href="#resume">
+        <a class="bywr-sidebar-option" href="#resume">
             <i class="fas fa-file-contract"></i><span><?= $_resume; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
-        <a class="sidebar-option" href="#contact">
+        <a class="bywr-sidebar-option" href="#contact">
             <i class="fas fa-paper-plane"></i><span><?= $_contact; ?></span><i class="fas fa-angle-right ms-auto"></i>
         </a>
     </div>
-    <div class="sidebar-content accordion-item flex-grow-0">
+    <div class="bywr-sidebar-content accordion-item flex-grow-0">
         <button class="accordion-header accordion-button p-2o5 collapsed" data-bs-toggle="collapse" data-bs-target="#lang-drop" aria-expanded="false" aria-controls="lang-drop">
             <i class="fas fa-earth-americas"></i><span><?= $_langs; ?>:</span>
         </button>
-        <div id="lang-drop" class="accordion-collapse collapse bg-dark-transparent" data-bs-parent="#sidebar">
+        <div id="lang-drop" class="accordion-collapse collapse bg-dark-transparent" data-bs-parent="#bywr-sidebar">
             <div class="d-flex flex-row">
-                <a class="sidebar-option" href="<?= $ROOT_ROUTE; ?>es" title="Español"><img src="<?= $HOME_PATH; ?>/img/co.png" width="16px" height="12px" style="margin: 4px;" alt="" /> Español<i class="fas fa-angle-right ms-auto"></i></a>
-                <a class="sidebar-option" href="<?= $ROOT_ROUTE; ?>en" title="English"><img src="<?= $HOME_PATH; ?>/img/uk.png" width="16px" height="12px" style="margin: 4px;" alt="" /> English<i class="fas fa-angle-right ms-auto"></i></a>
+                <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE; ?>es" title="Español"><img src="<?= $HOME_PATH; ?>/img/co.png" width="16px" height="12px" style="margin: 4px;" alt="" /> Español<i class="fas fa-angle-right ms-auto"></i></a>
+                <a class="bywr-sidebar-option" href="<?= $ROOT_ROUTE; ?>en" title="English"><img src="<?= $HOME_PATH; ?>/img/uk.png" width="16px" height="12px" style="margin: 4px;" alt="" /> English<i class="fas fa-angle-right ms-auto"></i></a>
             </div>
         </div>
         <button class="accordion-header accordion-button p-2o5 collapsed" data-bs-toggle="collapse" data-bs-target="#theme-drop" aria-expanded="false" aria-controls="theme-drop">
             <i class="fas fa-palette"></i><span><?= $_themes; ?>:</span>
         </button>
-        <div id="theme-drop" class="accordion-collapse collapse bg-dark-transparent" data-bs-parent="#sidebar">
+        <div id="theme-drop" class="accordion-collapse collapse bg-dark-transparent" data-bs-parent="#bywr-sidebar">
             <div class="d-flex flex-row">
-                <a class="sidebar-option" href="javascript:document.getElementById('themesheet').setAttribute('href','css/v2.light.css');set_cookie('theme','light');"><i class="fas fa-sun"></i><?= $_light; ?><i class="fas fa-angle-right ms-auto"></i></a>
-                <a class="sidebar-option" href="javascript:document.getElementById('themesheet').setAttribute('href','css/v2.dark.css');set_cookie('theme','dark');"><i class="fas fa-moon"></i><?= $_dark; ?><i class="fas fa-angle-right ms-auto"></i></a>
+                <a class="bywr-sidebar-option" href="javascript:document.getElementById('themesheet').setAttribute('href','css/v2.light.css');set_cookie('theme','light');"><i class="fas fa-sun"></i><?= $_light; ?><i class="fas fa-angle-right ms-auto"></i></a>
+                <a class="bywr-sidebar-option" href="javascript:document.getElementById('themesheet').setAttribute('href','css/v2.dark.css');set_cookie('theme','dark');"><i class="fas fa-moon"></i><?= $_dark; ?><i class="fas fa-angle-right ms-auto"></i></a>
             </div>
         </div>
         <p class="m-0 p-2 border-top" style="font-size: 0.75rem;"><?= $_footer; ?></p>
     </div>
-    <a id="sidebar-toggle" class="sidebar-toggle" href="javascript:;" title="Alternar menu lateral"><i class="fas fa-bars"></i><span>menu</span></a>
-    <div id="sidebar-hidden" class="sidebar-hidden">
+    <a id="bywr-sidebar-toggle" class="bywr-sidebar-toggle" href="javascript:;" title="Alternar menu lateral"><i class="fas fa-bars"></i><span>menu</span></a>
+    <div id="bywr-sidebar-hidden" class="bywr-sidebar-hidden">
         <div class="navbar-brand has-background-contain mt-auto" style="height:48px;width:48px;background-image:url('<?= $HOME_PATH; ?>/img/logo.png');"></div>
     </div>
 </nav>
