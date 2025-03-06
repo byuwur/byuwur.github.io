38929e6f05854c7d4381f599888318caa16f8bfd
[Mateus] byUwUr <atrujillomateus@gmail.com>
2025-03-05 23:34:31 -0500

    This can be helpful.

--- _common.php
+++ _common.php
@@ -6,6 +6,7 @@
  * Copyright (c) 2025 Andrés Trujillo [Mateus] byUwUr
  */
 
+// This is an example of how you can use SPA.php files along side yours.
 require_once $TO_HOME . "spa.php/_common.php";
-
+// Just call the SPA.php file and add whatever you need below
 if (file_exists($TO_HOME . "lang/" . $app_lang . ".php")) require_once $TO_HOME . "lang/" . $app_lang . ".php";
--- _plugins.php
+++ _plugins.php
@@ -6,4 +6,6 @@
  * Copyright (c) 2025 Andrés Trujillo [Mateus] byUwUr
  */
 
+// This is an example of how you can use SPA.php files along side yours.
 require_once $TO_HOME . "spa.php/_plugins.php";
+// Just call the SPA.php file and add whatever you need below
