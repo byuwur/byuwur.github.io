40ebaeb27c237834ff9411acac5a79df89450066
Andrés Trujillo Mateus <atrujillomateus@gmail.com>
2026-08-14 05:47:44 -0500

    [Hotfix] Improve dark theme red.

--- js/v2.js
+++ js/v2.js
@@ -8,30 +8,4 @@ $(() => {
 			loop: true,
 			strings: ["PHP", "Javascript - Typescript", "Bootstrap - Tailwind", "React - React Native", "MySQL", "Git", "CI/CD", "Java", "Vegas Pro", "Adobe Premiere Pro", "Adobe Photoshop & Lightroom", "Davinci Resolve"]
 		});
-	// Section scroll on non-scrollable body
-	/* 
-	$("section").each(function () {
-		$(this)
-			.off("wheel")
-			.on("wheel", function (e) {
-				const delta = e.originalEvent.deltaY;
-				const scrollSum = $(this).scrollTop() + $(this).innerHeight();
-				if (delta > 0) {
-					if (scrollSum < this.scrollHeight) return;
-					const nextSection = $(this).next("section");
-					if (!$(nextSection).length) return;
-					$(`html, body, ${byCommon.APP_CONTAINER_SELECTOR ?? ""}`)
-						.stop()
-						.animate({ scrollTop: nextSection.offset().top - 1 }, 999, "easeInOutExpo");
-				} else if (delta < 0) {
-					if (scrollSum > $(this).innerHeight()) return;
-					const prevSection = $(this).prev("section");
-					if (!$(prevSection).length) return;
-					$(`html, body, ${byCommon.APP_CONTAINER_SELECTOR ?? ""}`)
-						.stop()
-						.animate({ scrollTop: prevSection.offset().top - 1 }, 999, "easeInOutExpo");
-				}
-			});
-	}); 
-	*/
 });
