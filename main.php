c829bcede12c569ae967da4ecb34b867e06c4c96
Andrés Trujillo Mateus <atrujillomateus@gmail.com>
2026-08-14 10:51:16 -0500

    Badges need space to breath.

--- main.php
+++ main.php
@@ -86,7 +86,7 @@ foreach ($LANG["skills.sections"] as $skillSection)
           </h4>
           <ul class="d-flex flex-wrap gap-2 mb-0">
             <?php foreach ($skillsection["items"] as $skill) { ?>
-              <li class="skill p-1 pb-0" data-bs-toggle="tooltip" data-bs-title="<?= htmlspecialchars($skill["years"], ENT_QUOTES, "UTF-8") . "+ " . htmlspecialchars($LANG["years"], ENT_QUOTES, "UTF-8")?>">
+              <li class="skill p-2 pb-0" data-bs-toggle="tooltip" data-bs-title="<?= htmlspecialchars($skill["years"], ENT_QUOTES, "UTF-8") . "+ " . htmlspecialchars($LANG["years"], ENT_QUOTES, "UTF-8")?>">
                 <h4 class="m-0"><i class="<?= htmlspecialchars($skill["icon"], ENT_QUOTES, "UTF-8") ?>"></i><?= htmlspecialchars($skill["text"], ENT_QUOTES, "UTF-8") ?></h4>
                 <div class="progress rounded-pill"><div class="progress-bar" style="width: <?= htmlspecialchars($skill["progress"], ENT_QUOTES, "UTF-8") ?>%"></div></div>
               </li>
